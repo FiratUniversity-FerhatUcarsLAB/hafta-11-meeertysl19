e553c6efa31d510b57f52b95c49bd7f77d28c2f3
meeertysl19 <250542017@firat.edu.tr>
2025-12-03 21:46:07 +0300

    Update Exercise4_3.java

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -1,24 +1,24 @@
 public class Exercise4_3 {
 
     public static void zoop() {
-        baffle();
-        System.out.print("You wugga ");
-        baffle();
+        baffle();                       // 3
+        System.out.print("You wugga "); // 6
+        baffle();                       // 7
     }
 
     public static void main(String[] args) {
-        System.out.print("No, I ");
-        zoop();
-        System.out.print("I ");
-        baffle();
+        System.out.print("No, I ");     // 1
+        zoop();                         // 2
+        System.out.print("I ");         // 10
+        baffle();                       // 11
     }
 
     public static void baffle() {
-        System.out.print("wug");
-        ping();
+        System.out.print("wug");        // 4, 8, 12
+        ping();                         // 5, 9, 13
     }
 
     public static void ping() {
-        System.out.println(".");
+        System.out.println(".");        // 5, 9, 13
     }
 }
